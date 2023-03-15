620ca35ef559aaafeccf39298a802e22ef9b2476
Amine <amine.tchitooss@gmail.com>
2023-03-15 17:21:35 +0100

    tweek

--- src/TraderDashboard/Chart/Chart.tsx
+++ src/TraderDashboard/Chart/Chart.tsx
@@ -1,7 +1,115 @@
 import * as React from 'react';
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, Area, Tooltip, CartesianGrid } from 'recharts';
+import { format, parseISO } from 'date-fns';
+import { TooltipProps } from 'recharts';
 
-export default class Chart extends React.Component<any, any> {
-  render() {
-    return <div className="chart">Chart</div>;
+const CHART_COLORS = {
+  firstLineColor: '#0BCBFB',
+  secondLineColor: '#644AF3',
+  GridLine: '#EDF2F8',
+  AxisText: '#6D7988'
+};
+
+const AxisStyle = {
+  fill: CHART_COLORS.AxisText,
+  fontStyle: 'normal',
+  fontWeight: 600,
+  fontSize: '14px'
+};
+
+const graphData = [
+  { date: '2022-01-01T04:00:00Z', offer: 0, vehicle: 50 },
+  { date: '2022-02-01T04:00:00Z', offer: 20, vehicle: 33 },
+  { date: '2022-03-01T04:00:00Z', offer: 40, vehicle: 80 },
+  { date: '2022-04-01T04:00:00Z', offer: 53, vehicle: 58 },
+  { date: '2022-05-01T04:00:00Z', offer: 63, vehicle: 18 },
+  { date: '2022-06-01T04:00:00Z', offer: 53, vehicle: 28 },
+  { date: '2022-07-01T04:00:00Z', offer: 63, vehicle: 38 },
+  { date: '2022-08-01T04:00:00Z', offer: 43, vehicle: 88 },
+  { date: '2022-09-01T04:00:00Z', offer: 53, vehicle: 58 },
+  { date: '2022-10-01T04:00:00Z', offer: 63, vehicle: 68 },
+  { date: '2022-11-01T04:00:00Z', offer: 53, vehicle: 78 },
+  { date: '2022-12-01T04:00:00Z', offer: 55, vehicle: 28 }
+];
+
+interface DashboardProps {}
+interface DashboardState {}
+export default class Chart extends React.Component<DashboardProps, DashboardState> {
+  constructor(props: DashboardProps) {
+    super(props);
+    this.state = {};
   }
+
+  render() {
+    return (
+      <div className="chart">
+        <ResponsiveContainer width="100%" height={370}>
+          <AreaChart data={graphData} margin={{ top: 0, left: -30, right: 0, bottom: 0 }}>
+            <defs>
+              <linearGradient id="firstColor" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="50%" stopColor={CHART_COLORS.firstLineColor} stopOpacity={0.8} />
+                <stop offset="100%" stopColor={CHART_COLORS.firstLineColor} stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="secondColor" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="50%" stopColor={CHART_COLORS.secondLineColor} stopOpacity={0.8} />
+                <stop offset="100%" stopColor={CHART_COLORS.secondLineColor} stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid stroke={CHART_COLORS.GridLine} strokeWidth={2} />
+
+            <YAxis tick={AxisStyle} />
+
+            <XAxis
+              tick={AxisStyle}
+              dataKey="date"
+              tickLine={false}
+              axisLine={true}
+              tickFormatter={str => {
+                const date = new Date(str);
+                return format(date, 'MMM');
+              }}
+            />
+
+            {/* <Tooltip /> */}
+            <Tooltip cursor={{ stroke: '#444D58', strokeWidth: 2 }} content={<CustomTooltip />} />
+            <Area
+              type="monotone"
+              dataKey="offer"
+              strokeWidth={2}
+              stroke={CHART_COLORS.firstLineColor}
+              fillOpacity={0.2}
+              fill="url(#firstColor)"
+              activeDot={{ r: 7 }}
+            />
+            <Area
+              type="monotone"
+              dataKey="vehicle"
+              strokeWidth={2}
+              stroke={CHART_COLORS.secondLineColor}
+              fillOpacity={0.2}
+              fill="url(#secondColor)"
+              activeDot={{ r: 7 }}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
+
+function CustomTooltip({ active, payload, label }: TooltipProps<number, string>) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="tooltip">
+        <p className="tooltip__label">{format(parseISO(label), 'd, MMMM yyyy')}</p>
+        {payload &&
+          payload.map((data, index) => (
+            <div className="tooltip__data" style={{ color: data.color }} key={index.toString()}>
+              {data.value}
+            </div>
+          ))}
+      </div>
+    );
+  }
+  return null;
 }
