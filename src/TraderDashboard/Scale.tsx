40fb3b54a59893c3d41fc6fe41fa2b1c5a755e54
Amine <amine.tchitooss@gmail.com>
2023-03-15 18:01:32 +0100

    add scale custom

--- src/TraderDashboard/Scale.tsx
+++ src/TraderDashboard/Scale.tsx
@@ -1,32 +1,50 @@
 import * as React from 'react';
 
 export enum SCALE_CHOICE {
-  MONTHLY = 'MONTH',
-  WEEKLY = 'WEEK',
-  DAILY = 'DAY'
+  MONTHLY = 'Monthly',
+  WEEKLY = 'Weekly',
+  DAILY = 'Daily'
 }
 
 interface ScaleProps {
   selectScale: (choice: SCALE_CHOICE) => void;
 }
+interface ScaleState {
+  isOpen: boolean;
+  value: SCALE_CHOICE;
+}
 
-export default class Scale extends React.Component<ScaleProps, any> {
+export default class Scale extends React.Component<ScaleProps, ScaleState> {
   constructor(props: ScaleProps) {
     super(props);
+    this.state = {
+      isOpen: false,
+      value: SCALE_CHOICE.MONTHLY
+    };
   }
 
-  changeScale(choice: React.ChangeEvent<HTMLSelectElement>) {
-    const tt: SCALE_CHOICE = choice.target.value as SCALE_CHOICE;
-    this.props.selectScale(tt);
+  changeScale(choice: SCALE_CHOICE) {
+    this.props.selectScale(choice);
+    this.setState({ value: choice, isOpen: false });
+  }
+
+  toggleScale() {
+    this.setState({ isOpen: !this.state.isOpen });
   }
 
   render() {
     return (
-      <select aria-label="Scale" className="scaleFrame" onChange={d => this.changeScale(d)}>
-        <option value={SCALE_CHOICE.MONTHLY}>Monthly</option>
-        <option value={SCALE_CHOICE.WEEKLY}>Weekly</option>
-        <option value={SCALE_CHOICE.DAILY}>Daily</option>
-      </select>
+      <div className="scale" onClick={() => this.toggleScale()}>
+        <div className={this.state.isOpen ? 'scaleFrame' : 'closed scaleFrame'}>{this.state.value}</div>
+        {this.state.isOpen && (
+          <div className="scaleList">
+            <span className="emptySpan"></span>
+            <span onClick={() => this.changeScale(SCALE_CHOICE.MONTHLY)}>{SCALE_CHOICE.MONTHLY}</span>
+            <span onClick={() => this.changeScale(SCALE_CHOICE.WEEKLY)}>{SCALE_CHOICE.WEEKLY}</span>
+            <span onClick={() => this.changeScale(SCALE_CHOICE.DAILY)}>{SCALE_CHOICE.DAILY}</span>
+          </div>
+        )}
+      </div>
     );
   }
 }
