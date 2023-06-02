dcf37967570a06a2b6c27974d57c7efb2e40e711
Amine <amine.tchitooss@gmail.com>
2023-06-02 17:57:26 +0200

    format

--- src/TraderDashboard/TraderToolTip.tsx
+++ src/TraderDashboard/TraderToolTip.tsx
@@ -3,25 +3,24 @@ import { TooltipProps } from 'recharts';
 import { TooltipState } from 'recharts';
 import { format, parseISO } from 'date-fns';
 
-
-export interface ToolTipProps extends  TooltipProps<number, string> {}
+export interface ToolTipProps extends TooltipProps<number, string> {}
 export interface ToolTipState {}
 
 export class TraderTooltip extends React.Component<TooltipProps, TooltipState> {
-    render(): false | JSX.Element {
-      if (this.props.active && this.props.payload && this.props.payload.length) {
-        return (
-          <div className="customTooltip">
-            <p className="customTooltip__label">{format(parseISO(this.props.label), 'dd MMMM yyyy')}</p>
-            {this.props.payload &&
-              this.props.payload.map((data, index) => (
-                <div className="customTooltip__data" style={{ color: data.color }} key={index.toString()}>
-                  {data.value}
-                </div>
-              ))}
-          </div>
-        );
-      }
-      return null;
+  render(): false | JSX.Element {
+    if (this.props.active && this.props.payload && this.props.payload.length) {
+      return (
+        <div className="customTooltip">
+          <p className="customTooltip__label">{format(parseISO(this.props.label), 'dd MMMM yyyy')}</p>
+          {this.props.payload &&
+            this.props.payload.map((data, index) => (
+              <div className="customTooltip__data" style={{ color: data.color }} key={index.toString()}>
+                {data.value}
+              </div>
+            ))}
+        </div>
+      );
     }
+    return null;
+  }
 }
